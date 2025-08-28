e5728bff7fc0db3353f6536dd365d5886dba58fd
phantomvepic321 <aravfinnawin@gmail.com>
2025-08-28 20:01:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ class StudyPlanner {
     const dayData = this.getDayData(date);
 
     // Visual classes based on defaults
-    const defaultsComplete = dayData.study && dayData.waste && dayData.sleep;
+    const defaultsComplete = dayData.defaults.study && dayData.defaults.waste && dayData.sleep;
     const anyDefaults = dayData.defaults.studyGoal || dayData.defaults.wasteGoal || dayData.defaults.sleepGoal;
     if (defaultsComplete) {
       dayEl.classList.add('completed');   // map to green in CSS
